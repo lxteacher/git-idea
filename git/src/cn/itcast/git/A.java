2739d22f13a7782fab9534c2f0fc15efa3990ada
Your Name <you@example.com>
2018-11-11 09:52:36 +0800

    第四冷饮

---
+++ git/src/cn/itcast/git/A.java
@@ -0,0 +1,42 @@
+package cn.itcast.git;
+
+import java.util.Objects;
+
+public class A {
+
+
+
+
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        A a = (A) o;
+        return Objects.equals(id, a.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
+}
--- git/src/cn/itcast/git/HelloWorld.java
+++ git/src/cn/itcast/git/HelloWorld.java
@@ -1,13 +1,40 @@
 package cn.itcast.git;
 
+import jdk.nashorn.internal.ir.annotations.Reference;
+import org.apache.lucene.index.IndexWriter;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 /**
  * 测试Git使用
  */
+@Reference
 public class HelloWorld {
 
+    @Test
     public static void main(String[] args) {
-        System.out.println("使用IDEA第一次使用Git提交");
+
+        //IndexWriter indexWriter = new IndexWriter()
+
+
+        try {
+            System.out.println("使用IDEA第一次使用Git提交");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println("使用IDEA第二次使用Git提交");
         System.out.println("使用IDEA第三次使用Git提交");
+        ArrayList<Object> haha = new ArrayList<>();
+
+        for (Object o : haha) {
+
+        }
+        for (Object o : haha) {
+
+        }
+
+
+
     }
 }
